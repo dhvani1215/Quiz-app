a3fae5389066ae577ef5080c349423ae5bafce87
dhvani1215 <146220288+dhvani1215@users.noreply.github.com>
2024-09-08 20:43:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const showQuestion = (question) => {
 const startTimer = (time) => {
   timer = setInterval(() => {
     if (time === 3) {
-      playAdudio("countdown.mp3");
+      playAdudio("/Quiz-app/countdown.mp3");
     }
     if (time >= 0) {
       progress(time);
